4bf274254bc8e3f0a6f00dee4bc3d1b5bef1e2f9
Vincent Schol <schol_vincent@hotmail.com>
2021-12-08 23:43:03 +0100

    diversen

--- src/App.svelte
+++ src/App.svelte
@@ -1,68 +1,70 @@
 <script>
+	import Box from './Box.svelte';
 	export let name;
 </script>
 
 <main>
-	<h1>curriculum vitae</h1>
+	<Box>
+		<h2>curriculum vitae</h2>
+	</Box>
+	<Box>
+		<h4>Domeinen</h4>
+		<p>1. Plannings- en beheersoftware voor uitzenddiensten van personenwagenvervoer en locatiewerk.</p>
+		<p>2. hedgingsoftware voor grondstoffenhandel.</p>
+	</Box>
+	<Box>
+		<h4>Projecten 2021</h4>
+		<p>Docker</p>
+		<p>Svelte</p>
+	</Box>
+	<Box>
+		<h4>Projecten 2020</h4>
+		<p>proof of concept maken voor API: REST, GraphQL, gRPC</p>
+		<p>proof of concept maken voor frontend in .NET Core: Razor Pages, MVC, Blazor</p>
+		<p>ontsluiten legacy .net 4.6 code in een gRPC- en GraphQL-service</p>
+	</Box>
+	<Box>
+		<h4>Projecten 2016 - 2019</h4>
+		<p>- opzetten CI/CD pipeline voor verschillende applicatietypes in TFS</p>
+		<p>- workItem-synchronisatie tussen TFS en monday.com</p>
+		<p>- integratie tussen MoreApp-formulieren en backendsysteem</p>
+		<p>- integratie tussen AFAS en backendsysteem</p>
+		<p>- migratie van legacy database/software naar Entity Framework en hogere .net versie</p>
+		<p>- verplaatsen rekeninstensieve query naar SQL Azure en tabel vullen met bulkinsert</p>
+		<p>- Single Sign On (SSO) voor drie websites realiseren met eigen autorisatieserver</p>
+		<p>- integratie van een 3d party (Python-)planningsalgoritme met backendsysteem</p>
+		<p>- ontwerpen en bouwen van een algoritme voor planning van locatiewerk</p>
+		<p>- toevoegen en refactoren van diverse functionaliteit aan bestaande software</p>
+		<h3>Analyse en ontwerp</h3>
+		<p>- aandacht voor onderhoudbaarheid, testbaarheid, beveiliging, stabiliteit, performance, duidelijk ontwerp / design patterns, logging en documentatie.</p>
+		<p>opstellen van specificaties en urenbegroting</p>
+		<p>Test Driven uitwerking van gedetailleerde specs</p>
+		<h4>GUI ontwerp</h4>
+		<h4>DevOps en projectplanning</h4>
+		<p>Build- en Release-omgeving inrichten onderhouden in TFS 2015/’18</p>
+		<p>REST-API en client libraries TFS, PowerShell, Branching & Merging in TFVC, FTP.</p>
+		<p>optimaliseren in TFS van het configuratiebeheer van de applicaties</p>
+		<p>installatie(scripts) van webapplicaties in IIS / installeren SSL certificaten</p>
+		<p>° Windows Server 2008/12/16 (basis DevOps gebruik)</p>
+		<p>° sprintplanning in TFS</p>
+		<h4>Ontwikkeling</h4>
+		<p>• Uitbouwen en onderhouden desktop-apps, websites / webservices en Windows Services</p>
+		<p>• unittesten en CodedUI-testen</p>
+		<p>• ASP.NET WebForms + MVC / WebAPI / OWIN / javascript (summier)</p>
+		<p>• SSO met IdentityServer3. Later gemigreerd naar IdentityServer4 / ASP.NET Core / Dapper.</p>
+		<p>• Windows Services / FireDaemon om Python scripts te hosten, alsmede enig onderhoud aan Python code zelf</p>
+		<p>• WinForms  (VB.NET en C#)</p>
+		<p>• SQL Server 2012/16 / Entity Framework / LINQ</p>
+		<p>• Google Docs API</p>
+		<p>• SpreadheetLight</p>
+		<p>• Azure (SQL / Storage)</p>
+		<p>• Unity + Simple Injector / SeriLog</p>
+		<p>• diversen, o.a. RegEx, xpath, xml/json parsing</p>
+	</Box>
+	<Box>
+		<p>hier komen de projecten van 1994 - 2016</p>
+	</Box>
 
-	Domeinen
-	1. Plannings- en beheersoftware voor uitzenddiensten van personenwagenvervoer en locatiewerk.
-	2. hedgingsoftware voor grondstoffenhandel.
-
-
-
-	2020
-	Projecten
-	° proof of concept maken voor API: REST, GraphQL, gRPC
-	° proof of concept maken voor frontend in .NET Core: Razor Pages, MVC, Blazor
-	° ontsluiten legacy .net 4.6 code in een gRPC- en GraphQL-service
-
-	Analyse en ontwerp
-	°
-	DevOps en projectplanning
-	°
-
-	Ontwikkeling
-	° ASP.NET Core
-
-
-	2016 - 2019
-	Projecten
-	° opzetten CI/CD pipeline voor verschillende applicatietypes in TFS
-	° workItem-synchronisatie tussen TFS en monday.com
-	° integratie tussen MoreApp-formulieren en backendsysteem
-	° integratie tussen AFAS en backendsysteem
-	° migratie van legacy database/software naar Entity Framework en hogere .net versie
-	° verplaatsen rekeninstensieve query naar SQL Azure en tabel vullen met bulkinsert
-	° Single Sign On (SSO) voor drie websites realiseren met eigen autorisatieserver
-	° integratie van een 3d party (Python-)planningsalgoritme met backendsysteem
-	° ontwerpen en bouwen van een algoritme voor planning van locatiewerk
-	° toevoegen en refactoren van diverse functionaliteit aan bestaande software
-	Analyse en ontwerp
-	° aandacht voor onderhoudbaarheid, testbaarheid, veiligheid, stabiliteit, performance, duidelijk ontwerp / design patterns, logging en documentatie.
-	° opstellen van specificaties en urenbegroting
-	° Test Driven uitwerking van gedetailleerde specs
-	° GUI ontwerp
-	DevOps en projectplanning
-	° Build- en Release-omgeving inrichten onderhouden in TFS 2015/’18
-	° REST-API en client libraries TFS, PowerShell, Branching & Merging in TFVC, FTP.
-	° optimaliseren in TFS van het configuratiebeheer van de applicaties
-	° installatie(scripts) van webapplicaties in IIS / installeren SSL certificaten
-	° Windows Server 2008/12/16 (basis DevOps gebruik)
-	° sprintplanning in TFS
-	Ontwikkeling
-	° Uitbouwen en onderhouden desktop-apps, websites / webservices en Windows Services
-	° unittesten en CodedUI-testen
-	° ASP.NET WebForms + MVC / WebAPI / OWIN / javascript (summier)
-	° SSO met IdentityServer3. Later gemigreerd naar IdentityServer4 / ASP.NET Core / Dapper.
-	° Windows Services / FireDaemon om Python scripts te hosten, alsmede enig onderhoud aan Python code zelf
-	° WinForms  (VB.NET en C#)
-	° SQL Server 2012/16 / Entity Framework / LINQ
-	° Google Docs API
-	° SpreadheetLight
-	° Azure (SQL / Storage)
-	° Unity + Simple Injector / SeriLog
-	° diversen, o.a. RegEx, xpath, xml/json parsing
 
 	<p>
 		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.
@@ -72,7 +74,7 @@
 
 <style>
 	main {
-		text-align: center;
+		text-align: left;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
